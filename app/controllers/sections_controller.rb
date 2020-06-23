d2d8a89b354254c6e12dfa25ef94ba0a3a2d5872
dfert1987 <github email address>
2020-06-23 15:18:44 -0600

    stadiums and sections no have controllers

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,17 @@
 class SectionsController < ApplicationController
+
+    def index
+        @sections = Section.all
+        render json: @sections
+    end
+
+    def show
+        @section = Section.find(params[:id])
+        render json: @section
+    end
+
+    private
+    def stadium_params
+        params.require(:section).permit(:name, :stadium_id, :image)
+    end
 end
--- app/controllers/stadia_controller.rb
+++ app/controllers/stadia_controller.rb
@@ -1,2 +1,16 @@
 class StadiaController < ApplicationController
+    def index
+        @stadiums = Stadium.all
+        render json: @stadiums
+    end
+
+    def show
+        @stadium = Stadium.find(params[:id])
+        render json: @stadium
+    end
+
+    private
+    def stadium_params
+        params.require(:stadium).permit(:name, :team, :city, :image, :logo, :capacity, :division)
+    end
 end
