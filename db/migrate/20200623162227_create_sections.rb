e23c3049dbab8e8bad7c1904eca7e69b9bd7d030
dfert1987 <github email address>
2020-07-01 12:46:57 -0600

    sections updated to have 1-15 numbers

--- db/migrate/20200623162227_create_sections.rb
+++ db/migrate/20200623162227_create_sections.rb
@@ -1,7 +1,7 @@
 class CreateSections < ActiveRecord::Migration[6.0]
   def change
     create_table :sections do |t|
-      t.string :name
+      t.integer :number
       t.string :image
       t.references :stadia, null: false, foreign_key: true
       t.timestamps
