1c23b683734479d730aa525c052b0c725adf6544
dfert1987 <github email address>
2020-07-02 20:46:41 -0600

    fixed controller for restaurants. now all working in postman

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
     def index
         @restaurants = Restaurant.all
-        render json: @restaurants, include: [:stadium]
+        render json: @restaurants, include: [:section]
     end
 
     def show
@@ -11,17 +11,7 @@ class RestaurantsController < ApplicationController
 
     private
     def stadium_params
-        params.require(:restaurant).permit(:name, :description, :mainCategory, :subCategory, :localSpecialty, :image, :stadium_id)
+        params.require(:restaurant).permit(:name, :description, :mainCategory, :subCategory, :localSpecialty, :image, :section_id)
     end
 end
 
-t.string "name"
-t.text "description"
-t.string "mainCategory"
-t.string "subCategory"
-t.boolean "localSpecialty"
-t.string "image"
-t.integer "section_id", null: false
-t.datetime "created_at", precision: 6, null: false
-t.datetime "updated_at", precision: 6, null: false
-t.index ["section_id"], name: "index_restaurants_on_section_id"
